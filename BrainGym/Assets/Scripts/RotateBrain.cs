669cf6b68a547e5c61fabb83e3624a9832975823
rishik840 <rhul840@gmail.com>
2016-05-21 10:47:26 -0600

    Added rotation to brain and changed basic style

---
+++ BrainGym/Assets/Scripts/RotateBrain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.Rotate(Vector3.up*Time.deltaTime*25.0f);    
+	}
+}
