3a2b40508c4449b15d6962008d95223da2760e1b
rishik840 <rhul840@gmail.com>
2016-05-21 11:25:26 -0600

    Added UI button and fading script

---
+++ BrainGym/Assets/Scripts/FadeInOutText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeInOutText : MonoBehaviour {
+    public float FadeoutTime;
+    private Color startColor;
+    private float timer;   
+
+	// Use this for initialization
+	void Start ()
+    {
+        FadeoutTime = 1.0f;
+        startColor = GetComponent<Text>().color;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += FadeoutTime * Time.deltaTime;
+        if (timer < 5.0f)
+            GetComponent<Text>().color = Color.Lerp(startColor, Color.clear, FadeoutTime * Time.deltaTime);
+        else
+        {
+            timer = 0.0f;
+            GetComponent<Text>().color = Color.Lerp( Color.clear, startColor, FadeoutTime * Time.deltaTime);
+        }
+    }
+}
