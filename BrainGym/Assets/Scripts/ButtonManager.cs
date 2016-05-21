e1db6046ca1c32ec7f61d2fafc781db44f6285b0
rishik840 <rhul840@gmail.com>
2016-05-21 11:34:44 -0600

    Added new Workout Scene and made scene transition

---
+++ BrainGym/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void LoadWorkoutLevel()
+    {
+        Application.LoadLevel("Workout");
+    }
+}
